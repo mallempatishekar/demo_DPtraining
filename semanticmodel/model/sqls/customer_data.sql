f3154f2c3ec45c1f6367a4880d79f54530b9b624
mallempatishekar <mallempatishekar@gmail.com>
2026-03-23 16:54:04 +0530

    add semantic model with user group

---
+++ semanticmodel/model/sqls/customer_data.sql
@@ -0,0 +1,9 @@
+SELECT
+    customer_id,
+    birth_year,
+    education,
+    marital_status,
+    income,
+    country,
+    created_at
+FROM icebase.training.customer_data;
---
+++ semanticmodel/model/sqls/purchase_data.sql
@@ -0,0 +1,15 @@
+SELECT
+    customer_id,
+    purchase_date,
+    recency,
+    numdealspurchases,
+    numwebpurchases,
+    numcatalogpurchases,
+    numstorepurchases,
+    mntwines,
+    mntmeatproducts,
+    mntfishproducts,
+    mntsweetproducts,
+    mntgoldprods,
+    mntfruits
+FROM icebase.training.purchase_data;
